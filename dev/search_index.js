277fc46035b7fb220f7d6d3537c39275583573c5
Documenter.jl <documenter@juliadocs.github.io>
2022-05-02 20:09:57 +0000

    build based on 112e2e3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GeometricAlgebra.jl","page":"Home","title":"GeometricAlgebra.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"GeometricAlgebra.jl defines a collection of abstract types and operations intended to serve as a common interface for geometric algebra packages. Our aim is to promote consistency across geometric algebra implementations that may be designed to serve different purposes and audiences.","category":"page"}]
+}
